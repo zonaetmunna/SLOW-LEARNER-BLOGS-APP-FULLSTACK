31dd5f63ffd4f0b7ec3008a36559973db94a1499
zonaetmunna <zonaetmonna@gmail.com>
2023-02-09 17:26:23 +0600

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
+import Counter from '@/features/counter/Counter'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Counter/>
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
